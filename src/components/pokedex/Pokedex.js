a11ac85d127caa48d82520c535fced40aa291d4e
David Iruzubieta <david.iruzubieta@ezzing.com>
2019-04-16 21:48:19 +0200

    Sass available. Changed files to final format.

---
+++ src/components/pokedex/Pokedex.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Pokedex.sass';
+
+class Pokedex extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>TEST</h1>
+            </div>
+        );
+    }
+}
+
+export default Pokedex;

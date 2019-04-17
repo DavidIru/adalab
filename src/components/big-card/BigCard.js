adc24a72d4a7ac58fa6a2215971b46186ae10f60
David Iruzubieta <david.iruzubieta@ezzing.com>
2019-04-17 04:13:36 +0200

    Code style changes. New typo.

--- src/components/big-card/BigCard.js
+++ src/components/big-card/BigCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 import './BigCard.sass';
 import noImage from './noimage.png';
--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './Pokedex.sass';
 import Card from '../card/Card';
